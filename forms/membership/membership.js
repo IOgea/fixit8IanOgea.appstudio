37faa68f0498b0f209ff3f5c66e4516f17c56886
IOgea <Irogea@gmail.com>
2020-10-03 16:52:35 -0500

    completed first program

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,15 @@
-/*
-let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
 
+let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+
+
+btnSubmitName.onclick=function(){
+  if (members.includes(inptName.value) == True) {
+  lblMember.value = "You are already a member"
+}
+else {
+  lblMember.value = "You are not already a member, you have been added to the members"
+  members.push(inptName.value)
+}
+}
+
+console.log(members)
